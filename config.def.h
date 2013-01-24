73829085586d4b5e58c073d35dc5d4a6198a9a01
Christopher Jeffrey <chjjeffrey@gmail.com>
2013-01-23 22:41:11 -0600

    master size

--- config.def.h
+++ config.def.h
@@ -58,7 +58,8 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact      = 0.55; /* factor of master area size [0.05..0.95] */
+//static const float mfact      = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact      = 0.50; /* factor of master area size [0.05..0.95] */
 static const int nmaster      = 1;    /* number of clients in master area */
 static const Bool resizehints = False; /* True means respect size hints in tiled resizals */
 
