362277caa666d22137beed41faf633f3580baeec
Christopher Jeffrey <chjjeffrey@gmail.com>
2013-01-24 08:53:15 -0600

    redraw centered window

--- center.c
+++ center.c
@@ -5,10 +5,8 @@ void center(const Arg *arg) {
   if (!c || !c->isfloating) return;
   //if (!ISVISIBLE(c)) return;
 
-  //c->x = (c->mon->ww / 2) - (c->w / 2);
-  //c->y = (c->mon->wh / 2) - (c->h / 2);
-  //arrange(selmon);
-
   c->x = m->mx + (m->mw / 2 - WIDTH(c) / 2);
   c->y = m->my + (m->mh / 2 - HEIGHT(c) / 2);
+
+  arrange(selmon);
 }
