e6e1e70101361b0a5cba2b7a316d130bd5546f0b
Christopher Jeffrey <chjjeffrey@gmail.com>
2013-01-24 21:00:24 -0600

    focus urgent

--- focusurgent.c
+++ focusurgent.c
@@ -1,24 +1,9 @@
 void
 focusurgent(const Arg *arg) {
-  Client *c = NULL;
-  for (c = selmon->clients; c; c = c->next) {
-    //if (ISVISIBLE(c) && c->isurgent) {
-    if (c->isurgent) {
-      Arg a = {.ui = c->tags};
-      view(&a);
-      focus(c);
-    }
-  }
-}
-
-#if 0
-void
-focusurgent(const Arg *arg) {
   Monitor *m = NULL;
   Client *c = NULL;
 	for (m = mons; m; m = m->next) {
     for (c = m->clients; c; c = c->next) {
-      //if (ISVISIBLE(c) && c->isurgent) {
       if (c->isurgent) {
         selmon = m;
         Arg a = {.ui = c->tags};
@@ -28,4 +13,3 @@ focusurgent(const Arg *arg) {
     }
   }
 }
-#endif
