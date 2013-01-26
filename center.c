b629e47bfe0a7743a187b5ad4c9ba5fd5267a34d
Christopher Jeffrey <chjjeffrey@gmail.com>
2013-01-26 16:53:24 -0600

    cleanup. formatting.

--- center.c
+++ center.c
@@ -1,12 +1,12 @@
-void center(const Arg *arg) {
-  Client *c = selmon->sel;
-  Monitor *m = c->mon;
+void
+center(const Arg *arg) {
+	Client *c = selmon->sel;
+	Monitor *m = c->mon;
 
-  if (!c || !c->isfloating) return;
-  //if (!ISVISIBLE(c)) return;
+	if (!c || !c->isfloating) return;
 
-  c->x = m->mx + (m->mw / 2 - WIDTH(c) / 2);
-  c->y = m->my + (m->mh / 2 - HEIGHT(c) / 2);
+	c->x = m->mx + (m->mw / 2 - WIDTH(c) / 2);
+	c->y = m->my + (m->mh / 2 - HEIGHT(c) / 2);
 
-  arrange(selmon);
+	arrange(selmon);
 }
--- focusurgent.c
+++ focusurgent.c
@@ -1,15 +1,15 @@
 void
 focusurgent(const Arg *arg) {
-  Monitor *m = NULL;
-  Client *c = NULL;
+	Monitor *m = NULL;
+	Client *c = NULL;
 	for (m = mons; m; m = m->next) {
-    for (c = m->clients; c; c = c->next) {
-      if (c->isurgent) {
-        selmon = m;
-        Arg a = {.ui = c->tags};
-        view(&a);
-        focus(c);
-      }
-    }
-  }
+		for (c = m->clients; c; c = c->next) {
+			if (c->isurgent) {
+				selmon = m;
+				Arg a = {.ui = c->tags};
+				view(&a);
+				focus(c);
+			}
+		}
+	}
 }
