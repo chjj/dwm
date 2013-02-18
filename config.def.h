f4a67c04c0a6491a6bb2d83792159e3c10d61c1f
Christopher Jeffrey <chjjeffrey@gmail.com>
2013-02-17 20:29:06 -0600

    remove old key def.

--- config.def.h
+++ config.def.h
@@ -164,7 +164,6 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	//{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	//{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	TAGKEYS(                        XK_1,                      0)
